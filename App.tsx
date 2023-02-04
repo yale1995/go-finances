ea75ee733e5e6e5bd62800106f5c8a9259c5bf13
Yale Araujo <yale850@gmail.com>
2023-02-03 23:27:39 -0300

    feat: dashboard screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './components/Welcome';
+import React from 'react'
+import {Dashboard} from './src/screens/Dashboard'
 
-export default function App() {
+export default function Home() {
   return (
-    <View style={styles.container}>
-      <Welcome />
-    </View>
-  );
+    <Dashboard/>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
