5f8a23508f69e534fdb2fc273ff8ad3a32679d56
Yale Araujo <yale850@gmail.com>
2023-02-03 23:55:53 -0300

    fix: Container by styled components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import {Container} from './styles'
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Text>Dashboard Yale</Text>
+        </Container>
+    )
+}
