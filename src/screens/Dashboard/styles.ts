4400dcd7cd4f703202727d98c97a180e19ad07a7
Yale Araujo <yale850@gmail.com>
2023-02-05 17:17:15 -0300

    adding type to styled components theme

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,5 +4,5 @@ export const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
-background-color: ${(props) => props.theme.colors.primary};
+background-color: ${({theme}) => theme.colors.primary};
 `
