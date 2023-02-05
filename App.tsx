750e2c001ab4719e79712330b1104b30ef0d5df5
Yale Araujo <yale850@gmail.com>
2023-02-05 19:19:01 -0300

    style: splash screen until finish loading fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import theme from './src/global/styles/theme'
+import AppLoading from 'expo-app-loading'
 
 import { Dashboard } from './src/screens/Dashboard'
 import { ThemeProvider } from 'styled-components'
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
 
 export default function Home() {
+  const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium, Poppins_700Bold })
+
+  if(!fontsLoaded) <AppLoading/>
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
