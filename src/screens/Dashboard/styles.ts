b1f94ecf5cd0c87072c79b3a887d04e511d49d6a
Yale Araujo <yale850@gmail.com>
2023-02-05 19:19:39 -0300

    feat: new styled components

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,50 @@
 import styled from "styled-components/native";
-
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 export const Container = styled.View`
 flex: 1;
-justify-content: center;
-align-items: center;
 background-color: ${({ theme }) => theme.colors.background};
 `
+export const Header = styled.View`
+width: 100%;
+height: ${RFPercentage(42)}px;
 
-export const Title = styled.Text`
-font-size: 24px;
-font-weight: bold;
-color: ${({ theme }) => theme.colors.text}
+background: ${({ theme }) => theme.colors.primary};
+
+justify-content: center;
+align-items: center;
+flex-direction: row;
 `
+
+export const UserWrapper = styled.View`
+width: 100%;
+padding: 0 ${RFValue(24)}px;
+`;
+
+export const UserInfo = styled.View`
+flex-direction: row;
+align-items: center;
+`;
+
+export const Photo = styled.Image`
+width: ${RFValue(48)};
+height: ${RFValue(48)};
+
+border-radius: 10px;
+`;
+
+
+export const User = styled.View`
+margin-left: 17px;
+`;
+export const UserGreeting = styled.Text`
+color: ${({ theme }) => theme.colors.shape};
+font-size: ${RFValue(18)};
+
+font-family: ${({ theme }) => theme.fonts.regular};
+`;
+export const UserName = styled.Text`
+color: ${({ theme }) => theme.colors.shape};
+font-size: ${RFValue(18)};
+
+font-family: ${({ theme }) => theme.fonts.bold};
+`;
