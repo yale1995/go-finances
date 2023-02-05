74bfe5adebbdfe0c251d059593a627ae9bd8fd1f
Yale Araujo <yale850@gmail.com>
2023-02-05 17:20:24 -0300

    feat: title styled component

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,5 +4,11 @@ export const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
-background-color: ${({theme}) => theme.colors.primary};
+background-color: ${({ theme }) => theme.colors.background};
+`
+
+export const Title = styled.Text`
+font-size: 24px;
+font-weight: bold;
+color: ${({ theme }) => theme.colors.text}
 `
