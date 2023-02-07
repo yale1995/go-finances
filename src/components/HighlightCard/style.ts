4d027521f3bc9bdab4267852d9dfb9182df03a48
Yale Araujo <yale850@gmail.com>
2023-02-06 23:51:19 -0300

    styles: styling other components

--- src/components/HighlightCard/style.ts
+++ src/components/HighlightCard/style.ts
@@ -9,16 +9,44 @@ width: ${RFValue(300)}px;
 border-radius: ${RFValue(5)}px;
 padding: ${RFValue(19)}px ${RFValue(23)}px ${RFValue(42)}px;
 `
+
+
 export const Header = styled.View`
-`
-export const Title = styled.Text`
-`
-export const Icon = styled(Feather)`
-`
-export const Footer = styled.View`
-`
-export const Amount = styled.Text`
+flex-direction: row;
+justify-content: space-between;
 `
 
+
+export const Title = styled.Text`
+font-size: ${RFValue(14)}px;
+font-family: ${({ theme }) => theme.fonts.regular};
+
+color: ${({ theme }) => theme.colors.text_dark};
+`
+
+
+
+export const Icon = styled(Feather)`
+font-size: ${RFValue(40)}px;
+`
+
+
+export const Footer = styled.View`
+`
+
+
+export const Amount = styled.Text`
+font-family: ${({ theme }) => theme.fonts.medium};
+font-size: ${RFValue(32)}px;
+
+color: ${({ theme }) => theme.colors.text_dark};
+margin-top: 38px;
+`
+
+
 export const LastTransaction = styled.Text`
+font-size: ${RFValue(12)}px;
+font-family: ${({ theme }) => theme.fonts.regular};
+
+color: ${({ theme }) => theme.colors.text};
 `
